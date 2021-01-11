ffb8cd1920ebaf8cd1528e52128c4a5a4e45b5ca
Felipe <felipe.pereira.dev@gmail.com>
2021-01-11 12:22:14 -0300

    feat: create rounds

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let teamListString = ""
 let teamList = []
 let teamListResult = []
+let numberOfRounds = 0
 
 document.getElementById("team-list").addEventListener("change", (event) => {
   teamListString = event.target.value
@@ -10,6 +11,12 @@ document.getElementById("team-list-form").addEventListener("submit", (event) => 
   event.preventDefault()
 
   createGlobalTeamList()
+
+  if(teamList.length > 1) {
+    const rounds = createRounds(teamList)
+    numberOfRounds = rounds.length
+    console.log(rounds)
+  }
 })
 
 function createGlobalTeamList() {
@@ -32,4 +39,60 @@ function createGlobalTeamList() {
       totalGoals: 0
     }
   })
+}
+
+function createRounds(teams) {
+  if (teams.length % 2 == 1) {
+    teams.push(null);
+  }
+
+  const teamsCount = teams.length;
+  const numOfOneWayRounds = teamsCount - 1;
+  const half = teamsCount / 2;
+
+  const tournamentPairings = [];
+
+  const playerIndexes = teams.map((_, i) => i).slice(1);
+
+  // this will create one-way matches
+  for (let round = 0; round < numOfOneWayRounds; round++) {
+    const roundPairings = [];
+
+    const newPlayerIndexes = [0].concat(playerIndexes);
+
+    /* this will create a index (column) relation between the teams 
+      [0, 1, 2, 3]
+       |  |  |  |
+      [7, 6, 5, 4]
+    */
+    const firstHalf = newPlayerIndexes.slice(0, half);
+    const secondHalf = newPlayerIndexes.slice(half, teamsCount).reverse();
+
+    for (let i = 0; i < firstHalf.length; i++) {
+      roundPairings.push({
+        homeTeam: teams[firstHalf[i]],
+        awayTeam: teams[secondHalf[i]],
+        place: teams[firstHalf[i]].state,
+      });
+    }
+
+    // rotating the array
+    playerIndexes.push(playerIndexes.shift());
+
+    tournamentPairings.push(roundPairings);
+  }
+
+  // invert mandant to create two-way matches
+  for (let round = 0; round < numOfOneWayRounds; round++) {
+    const newReverseRounds = tournamentPairings[round].map(match => {
+      return {
+        homeTeam: match.awayTeam,
+        awayTeam: match.homeTeam,
+        place: match.awayTeam.state
+      }
+    })
+    tournamentPairings.push(newReverseRounds)
+  }
+
+  return tournamentPairings
 }
